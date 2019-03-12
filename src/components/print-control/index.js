37069b05fdacb26d3e4ed4a2e40fa559d5883487
Rob Gaston <rgaston@fargeo.com>
2019-03-12 13:52:12 -0700

    adds attribution to printed maps

--- src/components/print-control/index.js
+++ src/components/print-control/index.js
@@ -1,4 +1,7 @@
 import * as template from './template.html';
+import * as computedStyleToInlineStyle from 'computed-style-to-inline-style';
+
+console.log(computedStyleToInlineStyle)
 
 export default class PrintControl {
     onAdd(map){
@@ -8,21 +11,38 @@ export default class PrintControl {
         this.map = map;
         this.container = el;
 
-        let leftControls = map._container.querySelector('.mapboxgl-ctrl-bottom-left');
+        let mapboxLogo = map._container.querySelector('.mapboxgl-ctrl-logo');
+        computedStyleToInlineStyle(
+            mapboxLogo,
+            { recursive: true }
+        );
 
         let printData;
         this.container.onclick = function() {
             const mywindow = window.open('', 'BAAQMD Maps', 'height=600,width=800');
             const img = new Image();
+            const logo = new Image();
+            logo.src = mapboxLogo.style.backgroundImage.slice(
+                5,
+                mapboxLogo.style.backgroundImage.length - 2
+            )
+            console.log(logo.src);
             if (!printData) {
                 printData = map.getCanvas().toDataURL("image/jpeg");
             }
             img.src = printData;
             img.style.width = '100%';
+            logo.style.width = '100px';
 
-            mywindow.document.write('<html><head><title>Print</title>');
-            mywindow.document.write('</head><body >');
+            mywindow.document.write('<html><head><title>BAAQMD Maps</title>');
+            mywindow.document.write('</head><body>');
             mywindow.document.write(img.outerHTML);
+            mywindow.document.write(
+                '<div style="position:absolute;bottom:20px;left:20px;z-index:200">' + logo.outerHTML + '</div>' +
+                '<div style="float:right;font-family:sans-serif;position:absolute;bottom:20px;right:20px;z-index:200">' +
+                    '<span>© Mapbox</span>&nbsp;<span>© OpenStreetMap</span>' +
+                '</div>'
+            );
             mywindow.document.write('</body></html>');
 
             mywindow.document.close();
