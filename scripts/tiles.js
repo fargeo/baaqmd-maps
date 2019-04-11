3c5166a9d0fdef7cc136add0d21ba38fdced8f4a
Rob Gaston <rgaston@fargeo.com>
2019-04-11 10:08:41 -0700

    refined tile load process

--- scripts/tiles.js
+++ scripts/tiles.js
@@ -1,57 +1,69 @@
 const fs = require('fs');
-const fetch = require('node-fetch');
 const tippecanoe = require('tippecanoe');
 const MapboxClient = require('mapbox');
 const AWS = require('aws-sdk');
+const config = JSON.parse(fs.readFileSync('./src/config.json'));
 
-const mapboxKey = JSON.parse(
-    fs.readFileSync(`./secret.json`)
-).mapboxKey;
-const mapbox = new MapboxClient(mapboxKey);
 const tilesPath = './data/baaqmd.mbtiles';
-const userName = 'baaqmd-publicmaps';
-const tilesetId = 'c3867v6s';
 
-if (fs.existsSync(tilesPath)) fs.unlinkSync(tilesPath);
+let mapboxKey = '';
+try {
+    mapboxKey = JSON.parse(
+        fs.readFileSync('./secret.json')
+    ).mapboxKey;
+} catch (error) {
+    fs.writeFileSync('./secret.json', JSON.stringify({
+        mapboxKey: ''
+    }));
+}
+if (!mapboxKey) {
+    console.error('missing Mapbox secret key; add a key that has access to ' +
+        'the Mapbox uploads API to "mapboxKey" in "./secret.json"');
+} else {
+    const mapbox = new MapboxClient(mapboxKey);
 
-console.log('building mbtiles file...');
-tippecanoe(['./data/*.geojson'], {
-    output: tilesPath,
-    maximumZoom: 15,
-    dropDensestAsNeeded: true,
-    noLineSimplification: true,
-    detectSharedBorders: true,
-    dropRate: 1
-}, {
-    echo: true
-});
+    if (fs.existsSync(tilesPath)) fs.unlinkSync(tilesPath);
 
-console.log('mbtiles file ready... getting mapbox credentials...');
-mapbox.createUploadCredentials((err, cred) => {
-    var AWS = require('aws-sdk');
-    var s3 = new AWS.S3({
-        accessKeyId: cred.accessKeyId,
-        secretAccessKey: cred.secretAccessKey,
-        sessionToken: cred.sessionToken,
-        region: 'us-east-1'
+    console.log('building mbtiles file...');
+    tippecanoe(['./data/*.geojson'], {
+        output: tilesPath,
+        maximumZoom: 15,
+        dropDensestAsNeeded: true,
+        noLineSimplification: true,
+        detectSharedBorders: true,
+        dropRate: 1
+    }, {
+        echo: true
     });
 
-    console.log('mapbox credentials ready... uploading mbtiles...');
-    s3.putObject({
-        Bucket: cred.bucket,
-        Key: cred.key,
-        Body: fs.createReadStream(tilesPath)
-    }, (err, response) => {
-        if (err) throw err
+    console.log('mbtiles file ready... getting mapbox credentials...');
+    mapbox.createUploadCredentials((err, cred) => {
+        const s3 = new AWS.S3({
+            accessKeyId: cred.accessKeyId,
+            secretAccessKey: cred.secretAccessKey,
+            sessionToken: cred.sessionToken,
+            region: 'us-east-1'
+        });
 
-        console.log('mbtiles upload complete... updating tileset...');
-        mapbox.createUpload({
-            tileset: `${userName}.${tilesetId}`,
-            url: cred.url
+        console.log('mapbox credentials ready... uploading mbtiles...');
+        s3.putObject({
+            Bucket: cred.bucket,
+            Key: cred.key,
+            Body: fs.createReadStream(tilesPath)
         }, (err, response) => {
             if (err) throw err
 
-            console.log('tileset update started (may take a few minutes to complete)...');
+            console.log('mbtiles upload complete... updating tileset...');
+            mapbox.createUpload({
+                tileset: `${config.userName}.${config.tilesetId}`,
+                url: cred.url
+            }, (err, response) => {
+                if (err) throw err
+
+                console.log('tileset update started (this may take a few ' +
+                    'minutes to complete, see ' +
+                    'https://studio.mapbox.com/tilesets/ for details)...');
+            });
         });
     });
-});
+}
