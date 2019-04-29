f1974659616b736c463041b0f120fe6b807f72f6
Rob Gaston <rgaston@fargeo.com>
2019-04-29 13:25:29 -0700

    refactors styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,48 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== 'production';
 
-module.exports = {
-    output: {
-        path: __dirname + '/dist',
-        filename: 'baaqmd-maps.js',
-        library: 'baaqmdMaps',
-        libraryTarget: 'umd'
-    },
-    devServer: {
-        port: 3000,
-        contentBase: __dirname + '/dist',
-        inline: true
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: 'babel-loader'
-        }, {
-            test: /\.scss$/,
-            use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-        }, {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }, {
-            test: /\.html$/,
-            exclude: /node_modules/,
-            use: 'html-loader'
-        }, {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'fonts/'
-                }
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production';
+    return {
+        output: {
+            path: __dirname + '/dist',
+            filename: 'baaqmd-maps.js',
+            library: 'baaqmdMaps',
+            libraryTarget: 'umd'
+        },
+        devServer: {
+            port: 3000,
+            contentBase: __dirname + '/dist',
+            inline: true
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                use: 'babel-loader'
+            }, {
+                test: /\.scss$/,
+                use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }, {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }, {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: 'html-loader'
+            }, {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
             }]
-        }]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'baaqmd-maps.css'
-        })
-    ]
-};
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'baaqmd-maps.css'
+            })
+        ]
+    };
+}
