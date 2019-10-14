bdbb994b738e705e228ac70ec80164c8bd8ceab7
Rob Gaston <rgaston@fargeo.com>
2019-10-14 13:18:36 -0700

    adds ability to expand popup content

--- src/viewmodels/map-details-panel.js
+++ src/viewmodels/map-details-panel.js
@@ -2,21 +2,47 @@ import * as ko from 'knockout';
 import * as mapboxgl from 'mapbox-gl';
 import * as scrollForMoreTemplate from '../templates/scroll-for-more.html';
 
+let popupComponents = [];
+let popupData;
 export default function MapDetailsPanel(params) {
+    const popupComponent = params.mapType() + 'Popup';
     this.mapType = params.mapType;
     this.map = params.map;
     this.showInfoPanel = params.showInfoPanel;
     var setupMap = (map) => {
         if (this.getPopupData && this.popupLayers) {
+            if (this.popupTemplate && !popupComponents.includes(popupComponent)) {
+                ko.components.register(popupComponent, {
+                    viewModel: function() {
+                        if (popupData) {
+                            for (var key in popupData) {
+                                this[key] = popupData[key];
+                            }
+                        }
+                    },
+                    template: this.popupTemplate
+                });
+                popupComponents.push(popupComponent);
+            }
+
             let click = (e) => {
                 if (this.popupTemplate) {
                     this.popupTemplate += scrollForMoreTemplate;
                     const feature = e.features[0];
-                    let popupData = this.getPopupData(feature);
+                    popupData = this.getPopupData(feature);
                     if (popupData) {
+                        popupData.showInfoPanel = params.showInfoPanel;
+                        var expandButton = `<button
+                            class="mapboxgl-popup-expand-button"
+                            type="button"
+                            data-bind="click: function() {
+                                showInfoPanel('${popupComponent}');
+                            }">
+                                <i class="fas fa-window-maximize"></i>
+                        </button>`;
                         const p = new mapboxgl.Popup()
                             .setLngLat(e.lngLat)
-                            .setHTML(this.popupTemplate)
+                            .setHTML(expandButton + this.popupTemplate)
                             .addTo(map);
                         let popupBody = p._content.querySelector('.baaqmd-maps-popup');
                         popupData.scrolledToBottom = ko.observable(false);
