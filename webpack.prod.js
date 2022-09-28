e7432df2ba63fe44cf716283a451bc774b88ca91
Rob Gaston <rgaston@fargeo.com>
2022-09-28 12:00:51 -0700

    optimizes build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,15 @@
 const merge = require('webpack-merge');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
     optimization: {
-        minimizer: [new OptimizeCSSAssetsPlugin({})],
+        minimize: true,
+        minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
     },
     module: {
         rules: [{
